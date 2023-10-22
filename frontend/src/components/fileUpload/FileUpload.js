6f3271a1feec6107315e9eaca60835bca6f89427
Embaye <weldensieteklay@gmail.com>
2023-10-21 21:30:41 -0500

    added elevetion

--- frontend/src/components/fileUpload/FileUpload.js
+++ frontend/src/components/fileUpload/FileUpload.js
@@ -1,8 +1,7 @@
 import React, { useState, useRef } from 'react';
-import { Box, Typography, Button, FormControl, InputLabel, Select, TextField } from '@mui/material';
+import { Box, Typography, Button, FormControl, InputLabel, Select, MenuItem, TextField } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import Papa from 'papaparse';
-import CustomButton from '../common/ButtonComponent'; // Adjust the import path accordingly
 
 const StyledTitle = styled(Typography)(({ theme }) => ({
     color: 'white',
@@ -11,9 +10,9 @@ const StyledTitle = styled(Typography)(({ theme }) => ({
     backgroundColor: 'darkblue',
     fontWeight: 'bold',
     textAlign: 'center',
-    boxShadow: theme.shadows[8],
     width: '95%',
-    marginBottom: '26px', 
+    marginBottom: '26px',
+    boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.6)', 
 }));
 
 const ContentWrapper = styled(Box)(({ theme }) => ({
@@ -25,8 +24,7 @@ const ContentWrapper = styled(Box)(({ theme }) => ({
     padding: '20px',
     boxSizing: 'border-box',
     marginTop: '10px',
-    boxShadow: theme.shadows[8], 
-
+    boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.3)', 
 }));
 
 const ButtonContainer = styled(Box)(({ theme }) => ({
@@ -39,65 +37,22 @@ const ButtonSpacer = styled(Box)(({ theme }) => ({
     marginLeft: '16px',
 }));
 
-const res = [
-    {     
-    mean: 0.34,
-    standard_error: 0.024,
+const mlMethods = ['Method A', 'Method B', 'Method C'];
+const depVars = ['Variable 1', 'Variable 2', 'Variable 3'];
+const independentVars = ['IndepVar 1', 'IndepVar 2', 'IndepVar 3'];
 
-}]
 const FileUpload = () => {
     const [selectedFile, setSelectedFile] = useState(null);
     const [machineLearningMethod, setMachineLearningMethod] = useState('');
     const [dependentVariable, setDependentVariable] = useState('');
     const [independentVariables, setIndependentVariables] = useState([]);
-    const [predictionResult, setPredictionResult] = useState([])
+    const [predictionResult, setPredictionResult] = useState([]);
     const fileInputRef = useRef(null);
 
     const handleFileChange = (event) => {
         const file = event.target.files[0];
         setSelectedFile(file);
-        saveFileToIndexedDB(file);
         parseCSVFile(file);
-    };
-
-    const saveFileToIndexedDB = (file) => {
-        const request = window.indexedDB.open('FileDatabase', 1);
-
-        request.onerror = (event) => {
-            console.error('Database error: ', event.target.errorCode);
-        };
-
-        request.onsuccess = (event) => {
-            const db = event.target.result;
-
-            const transaction = db.transaction(['files'], 'readwrite');
-
-            const objectStore = transaction.objectStore('files');
-
-            const fileReader = new FileReader();
-
-            fileReader.onload = (event) => {
-                const fileData = event.target.result;
-
-                const addRequest = objectStore.add({ data: fileData, fileName: file.name });
-
-                addRequest.onsuccess = () => {
-                    console.log('File added to IndexedDB.');
-                };
-
-                addRequest.onerror = () => {
-                    console.error('Error adding file to IndexedDB.');
-                };
-            };
-
-            fileReader.readAsArrayBuffer(file);
-        };
-
-        request.onupgradeneeded = (event) => {
-            const db = event.target.result;
-            const objectStore = db.createObjectStore('files', { keyPath: 'fileName' });
-            console.log('Database setup complete.');
-        };
     };
 
     const parseCSVFile = (file) => {
@@ -116,34 +71,12 @@ const FileUpload = () => {
     };
 
     const handlePredict = () => {
-        // Code to send selected data to the backend (e.g., using fetch)
-        // Example: Sending a POST request to a backend endpoint
-        fetch('your_backend_endpoint', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                method: machineLearningMethod,
-                dependentVariable,
-                independentVariables,
-            }),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                // Handle the response from the backend
-                console.log('Prediction response:', data);
-                setPredictionResult(data)
-            })
-            .catch((error) => {
-                console.error('Error predicting:', error);
-            });
+        console.log(machineLearningMethod, dependentVariable, independentVariables, 'model inputs')
     };
 
     const handleClear = () => {
-        // Code to clear selected data
-        setMachineLearningMethod('');
-        setDependentVariable('');
+        setMachineLearningMethod([]);
+        setDependentVariable([]);
         setIndependentVariables([]);
     };
 
@@ -184,7 +117,11 @@ const FileUpload = () => {
                             onChange={(e) => setMachineLearningMethod(e.target.value)}
                             style={{ minWidth: '200px' }}
                         >
-                            {/* Add your machine learning methods here */}
+                            {mlMethods.map((method) => (
+                                <MenuItem key={method} value={method}>
+                                    {method}
+                                </MenuItem>
+                            ))}
                         </Select>
                     </FormControl>
                     <FormControl style={{ marginLeft: '16px' }}>
@@ -194,33 +131,51 @@ const FileUpload = () => {
                             onChange={(e) => setDependentVariable(e.target.value)}
                             style={{ minWidth: '200px' }}
                         >
+                            {depVars.map((variable) => (
+                                <MenuItem key={variable} value={variable}>
+                                    {variable}
+                                </MenuItem>
+                            ))}
                         </Select>
                     </FormControl>
+
                     <FormControl style={{ marginLeft: '16px' }}>
                         <InputLabel>Independent Variables</InputLabel>
                         <Select
-                            multiple
+                            multiple  
                             value={independentVariables}
                             onChange={(e) => setIndependentVariables(e.target.value)}
                             style={{ minWidth: '200px' }}
+                            renderValue={(selected) => selected.join(', ')}
                         >
+                            {independentVars.map((variable) => (
+                                <MenuItem key={variable} value={variable}>
+                                    {variable}
+                                </MenuItem>
+                            ))}
                         </Select>
                     </FormControl>
+
+
                 </Box>
                 <ButtonContainer>
-                    <CustomButton
-                        label="Predict"
+                    <Button
+                        variant="contained"
+                        color="primary"
                         onClick={handlePredict}
-                        width="150px"
-                    />
+                        style={{ width: '150px' }}
+                    >
+                        Predict
+                    </Button>
                     <ButtonSpacer />
-                    <CustomButton
-                        label="Clear"
+                    <Button
+                        variant="contained"
+                        color="secondary"
                         onClick={handleClear}
-                        width="150px"
-                        backgroundColor="secondary.main"
-
-                    />
+                        style={{ width: '150px' }}
+                    >
+                        Clear
+                    </Button>
                 </ButtonContainer>
             </ContentWrapper>
         </Box>
